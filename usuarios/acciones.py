cac9f9bae6fd19cc5b38c7611f63f4d4fc07335a
Ismael Dominguez <idominguez@kerneltechnologies.com>
2021-08-26 18:52:34 -0500

    Creación del modulo de usuarios, con acciones y usuario

---
+++ usuarios/acciones.py
@@ -0,0 +1,17 @@
+class Acciones():
+    """docstring for Acciones."""
+    
+    def registro(self):
+        print("Se va a registrar en el sistema....\n")
+
+        nombre = input("Ingresa tu nombre: ")
+        apellidoPaterno = input("Ingresa tu apellido paterno: ")
+        apellidoMaterno = input("Ingresa tu apellido materno: ")
+        email = input("Ingresa tu correo: ")
+        password = input("Ingresa tu contraseña: ")
+    
+    def login(self):
+        print("Por favor, identificate en el sistema....\n")
+
+        email = input("Ingresa tu correo: ")
+        password = input("Ingresa tu contraseña: ")
---
+++ usuarios/usuario.py
@@ -0,0 +1,10 @@
+class Usuario():
+    """docstring for Usuario."""
+
+    def __init__(self, nombre, apellidoPaterno, apellidoMaterno, email, password):
+        self.nombre = nombre
+        self.apellidoPaterno = apellidoPaterno
+        self.apellidoMaterno = apellidoMaterno
+        self.email = email
+        self.password = password
+        
